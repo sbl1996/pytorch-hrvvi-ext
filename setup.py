a7f90826657a6c01feca16e09a2655be71d11287
HrvvI <sbl1996@gmail.com>
2019-04-23 13:14:47 +0800

    Fix error in setup.py

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ EXTRAS = {
 # Except, perhaps the License and Trove Classifiers!
 # If you do change the License, remember to change the Trove Classifier for that!
 
-here = os.path.dirname(os.path.realpath(__file__))
+here = os.path.dirname(os.path.abspath(__file__))
 
 # Import the README and use it as the long-description.
 # Note: this will only work if 'README.md' is present in your MANIFEST.in file!
@@ -156,12 +156,6 @@ class UploadCommand(Command):
         sys.exit()
 
 
-PLATFORM = sys.platform
-if PLATFORM == 'darwin':
-    ext_compile_args = ["-g", "-stdlib=libc++", "-std=c++11"]
-else:
-    ext_compile_args = ["-g", "-std=c++11"]
-
 # Where the magic happens:
 setup(
     name=NAME,
@@ -184,14 +178,6 @@ setup(
     extras_require=EXTRAS,
     # include_package_data=True,
     license='MIT',
-    classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: Implementation :: CPython',
-    ],
     ext_modules=get_extensions(),
     # $ setup.py publish support.
     cmdclass={
