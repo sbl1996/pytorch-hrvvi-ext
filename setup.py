5703c8b54bcbd3fb29001d2c033a13a90c5618d1
HrvvI <sbl1996@gmail.com>
2019-04-23 12:11:24 +0800

    Fix error in setup.py

--- setup.py
+++ setup.py
@@ -180,7 +180,7 @@ setup(
     # },
     install_requires=REQUIRED,
     extras_require=EXTRAS,
-    include_package_data=True,
+    # include_package_data=True,
     license='MIT',
     classifiers=[
         # Trove classifiers
