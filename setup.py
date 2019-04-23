f7b1f0a9baa453b0ed83884da885b4b4dea25088
HrvvI <sbl1996@gmail.com>
2019-04-23 12:45:18 +0800

    Fix error in setup.py

--- setup.py
+++ setup.py
@@ -92,6 +92,8 @@ def get_extensions():
         extension = CUDAExtension
         sources += source_cuda
         define_macros += [('WITH_CUDA', None)]
+        extra_compile_args['nvcc'] = ['-DCUDA_HAS_FP16=1', '-D__CUDA_NO_HALF_OPERATORS__',
+                                      '-D__CUDA_NO_HALF_CONVERSIONS__', '-D__CUDA_NO_HALF2_OPERATORS__']
 
     sources = [os.path.join(extensions_dir, s) for s in sources]
 
