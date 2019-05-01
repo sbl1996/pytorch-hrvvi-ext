af41cd556c7e4d0504e52747bc2f29cdfddad3e2
HrvvI <sbl1996@gmail.com>
2019-05-01 11:57:20 +0800

    Change name

--- hutil/nn/__init__.py
+++ hutil/nn/__init__.py
@@ -1,2 +1,2 @@
-from hutil.nn.psroi_align import PSRoIAlign
+from hutil.nn.psroi_align import PSROIAlign, psroi_align
 # from hutil.nn.roi_align import RoIAlign
--- hutil/nn/psroi_align.py
+++ hutil/nn/psroi_align.py
@@ -9,17 +9,17 @@ from torch.nn.modules.utils import _pair
 from hutil import _C
 
 
-class _PSRoIAlign(Function):
+class _PSROIAlign(Function):
     @staticmethod
     def forward(ctx, input, roi, out_channels, output_size, spatial_scale, sampling_ratio):
         ctx.save_for_backward(roi)
         ctx.output_size = _pair(output_size)
         ctx.out_channels = out_channels
-        ctx.spatial_scale = _pair(spatial_scale)
+        ctx.spatial_scale = spatial_scale
         ctx.sampling_ratio = sampling_ratio
         ctx.input_shape = input.size()
         output = _C.psroi_align_forward(
-            input, roi, spatial_scale[0], spatial_scale[1], out_channels,
+            input, roi, spatial_scale, out_channels,
             output_size[0], output_size[1], sampling_ratio)
         return output
 
@@ -33,28 +33,24 @@ class _PSRoIAlign(Function):
         sampling_ratio = ctx.sampling_ratio
         bs, ch, h, w = ctx.input_shape
         grad_input = _C.psroi_align_backward(
-            grad_output, rois, spatial_scale[0], spatial_scale[1], out_channels,
+            grad_output, rois, spatial_scale, out_channels,
             output_size[0], output_size[1], bs, ch, h, w, sampling_ratio)
         return grad_input, None, None, None, None, None
 
 
-psroi_align = _PSRoIAlign.apply
+psroi_align = _PSROIAlign.apply
 
 
-class PSRoIAlign(nn.Module):
-    def __init__(self, out_channels, output_size, spatial_scale=None, sampling_ratio=2, adaptive=True):
+class PSROIAlign(nn.Module):
+    def __init__(self, out_channels, output_size, spatial_scale, sampling_ratio):
         super().__init__()
         self.out_channels = out_channels
         self.output_size = output_size
         self.spatial_scale = spatial_scale
         self.sampling_ratio = sampling_ratio
-        self.adaptive = adaptive
 
     def forward(self, input, rois):
-        spatial_scale = self.spatial_scale
-        if self.adaptive:
-            spatial_scale = tuple(input.size()[2:4])
-        return psroi_align(input, rois, self.out_channels, self.output_size, spatial_scale, self.sampling_ratio)
+        return psroi_align(input, rois, self.out_channels, self.output_size, self.spatial_scale, self.sampling_ratio)
 
     def __repr__(self):
         tmpstr = self.__class__.__name__ + '('
@@ -62,6 +58,5 @@ class PSRoIAlign(nn.Module):
         tmpstr += ', output_size=' + str(self.output_size)
         tmpstr += ', spatial_scale=' + str(self.spatial_scale)
         tmpstr += ', sampling_ratio=' + str(self.sampling_ratio)
-        tmpstr += ', adaptive=' + str(self.adaptive)
         tmpstr += ')'
         return tmpstr
