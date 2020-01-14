649648f7bcce2d8f7f369851aae38b52ba04ab50
Shen Biluo <sbl1996@126.com>
2020-01-14 18:29:09 +0800

    :art: Add deform_conv

--- horch/csrc/detection.cpp
+++ horch/csrc/detection.cpp
@@ -1,3 +1,4 @@
+#include "Deform.h"
 #include "IoUMN.h"
 #include "PSROIAlign.h"
 #include "ROIAlign.h"
@@ -14,4 +15,19 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("psroi_align_backward", &PSROIAlign_backward, "PSROIAlign_backward");
     m.def("roi_align_forward", &ROIAlign_forward, "ROIAlign_forward");
     m.def("roi_align_backward", &ROIAlign_backward, "ROIAlign_backward");
+#ifdef WITH_CUDA
+    m.def("deform_conv_forward_cuda", &deform_conv_forward_cuda,
+          "deform forward (CUDA)");
+    m.def("deform_conv_backward_input_cuda", &deform_conv_backward_input_cuda,
+          "deform_conv_backward_input (CUDA)");
+    m.def("deform_conv_backward_parameters_cuda",
+          &deform_conv_backward_parameters_cuda,
+          "deform_conv_backward_parameters (CUDA)");
+    m.def("modulated_deform_conv_cuda_forward",
+          &modulated_deform_conv_cuda_forward,
+          "modulated deform conv forward (CUDA)");
+    m.def("modulated_deform_conv_cuda_backward",
+          &modulated_deform_conv_cuda_backward,
+          "modulated deform conv backward (CUDA)");
+#endif
 }
