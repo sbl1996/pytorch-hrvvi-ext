7cb928166430b01542f7a14776b2a3f888d6b2a9
sbl1996 <sbl1996@126.com>
2020-05-15 00:06:01 +0800

    fix import

--- horch/models/cifar/__init__.py
+++ horch/models/cifar/__init__.py
@@ -5,3 +5,4 @@ from horch.models.cifar.shufflenetv2 import ShuffleNetV2
 from horch.models.cifar.efficientnet import EfficientNet
 from horch.models.cifar.efficientnetm import EfficientNet as EfficientNetM
 from horch.models.cifar.pyramidnet import PyramidNet
+from horch.models.cifar.regnet import RegNet
